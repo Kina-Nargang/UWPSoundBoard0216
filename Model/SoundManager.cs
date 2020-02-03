7da0f572deaa0a76eb15b8946d7323219c1a77ac
Kina <35324430+Kina-Nargang@users.noreply.github.com>
2020-02-03 13:15:38 -0800

    Initial commit

---
+++ Model/SoundManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWPSoundBoard.Model
+{
+    public static class SoundManager
+    {
+        private static List<Sound> getSounds()
+        {
+            var sounds = new List<Sound>();
+            sounds.Add(new Sound("Cow", SoundCategory.Animals));
+            sounds.Add(new Sound("Cat", SoundCategory.Animals));
+
+            sounds.Add(new Sound("Gun", SoundCategory.Catoons));
+            sounds.Add(new Sound("Spring", SoundCategory.Catoons));
+
+            sounds.Add(new Sound("Clock", SoundCategory.Tanuts));
+            sounds.Add(new Sound("LOL", SoundCategory.Tanuts));
+
+            sounds.Add(new Sound("Ship", SoundCategory.Warnings));
+            sounds.Add(new Sound("Siren", SoundCategory.Warnings));
+
+            return sounds;
+        }
+    }
+}
